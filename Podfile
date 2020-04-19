65c999313cb9acf4bd16c3fd93adbbc9d02f3d5a
yaiwamoto <hcmnorgir0808@yahoo.co.jp>
2020-04-20 02:48:52 +0900

    メールアドレス/パスワードによる認証

---
+++ Podfile
@@ -0,0 +1,20 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'iOS-FirebaseSample' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+  pod 'Firebase/Core'
+  pod 'Firebase/Auth'
+  # Pods for iOS-FirebaseSample
+
+  target 'iOS-FirebaseSampleTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'iOS-FirebaseSampleUITests' do
+    # Pods for testing
+  end
+
+end
--- iOS-FirebaseSample/ViewController.swift
+++ iOS-FirebaseSample/ViewController.swift
@@ -7,14 +7,46 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var email: UITextField!
+    @IBOutlet weak var pass: UITextField!
+    
+    var changed: AuthStateDidChangeListenerHandle?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        label.text = "email & pass"
+        pass.isSecureTextEntry = true
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        changed = Auth.auth().addStateDidChangeListener { (auth, user) in
+            self.label.text = user?.email
+        }
     }
 
-
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(true)
+        
+        if let changed = changed {
+            Auth.auth().removeStateDidChangeListener(changed)
+        }
+    }
+    
+    @IBAction func doAction(_ sender: Any) {
+        let email = self.email.text
+        let pass = self.pass.text
+        Auth.auth().signIn(withEmail: email ?? "", password: pass ?? "") { (user, error) in
+            if error != nil {
+                self.label.text = error?.localizedDescription
+                return
+            }
+        }
+    }
+    
 }
 
